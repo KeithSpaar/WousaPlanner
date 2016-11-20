c9cdf839b6a881c7dbe67a001a81c1a41ae8168a
KeithSpaar <ks5xq@virginia.edu>
2016-11-19 20:28:45 -0500

    Added names and a RecyclerView

--- app/src/main/java/com/wousa/eisenhowergrid/MainActivity.java
+++ app/src/main/java/com/wousa/eisenhowergrid/MainActivity.java
@@ -2,12 +2,18 @@ package com.wousa.eisenhowergrid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+//Keith Spaar and Chris Rickey
+
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView EventRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EventRecyclerView = (RecyclerView) findViewById(R.id.EventRecyclerView);
     }
 }
